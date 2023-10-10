3f41c5ed304c9dd9960873a5bb852f346746f6ae
Alex Young <alexanderjyoung15@gmail.com>
2023-10-10 16:56:31 -0400

    validated second question

--- Lab 5/Program.cs
+++ Lab 5/Program.cs
@@ -7,8 +7,6 @@
             string[] names = { "Tommy", "Ethan", "Alex", "Irving", "Kyle", "Michael" };
             string[] hometown = { "Detroit", "New York", "San Jose", "Columbus", "Canada", "Royal Oak" };
             string[] favfood = { "Culver's", "Steak", "Soup", "Salad", "Pasta", "Breadsticks" };
-            //int x = 0;
-            //while (x == 0)
             {
                 while (true)
 
@@ -29,22 +27,29 @@
                     Console.WriteLine("Student Name: " + names[index]);
 
                     Console.WriteLine($"Learn more about {names[index]}  type 'Hometown' or 'Favorite food'");
-                    string choice = Console.ReadLine();
-
-                    if (choice.ToLower() == "hometown")
-                    {
-                        Console.WriteLine($"{names[index]} is from {hometown[index]}");
-                    }
-                    else if (choice.ToLower() == "favorite food")
-                    {
-                        Console.WriteLine($"{names[index]}'s favorite food is {favfood[index]}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"I don't know that about {names[index]}. Try 'Hometown' or 'Favorite food");
-                    }
 
 
+                    string category = null;
+                    while (category == null)
+                    {
+                        category = Console.ReadLine();
+
+
+                        if (category.ToLower() == "hometown")
+                        {
+                            Console.WriteLine($"{names[index]} is from " + hometown[index]);
+                        }
+                        else if (category.ToLower() == "favorite food")
+                        {
+                            Console.WriteLine($"{names[index]}'s favorite food is " + favfood[index]);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"I don't know that about {names[index]}. Try 'Hometown' or 'Favorite food");
+                            category = null;
+                        }
+
+                    }
 
 
                     Console.Write("Would you like to learn about another student? Type 'y/n'");
@@ -55,15 +60,16 @@
                         break;
                     }
                 }
-                //Console.Write("Would you like to learn about another student? Type 'y/n'");
-                //string goagain = Console.ReadLine();
-                //if (goagain != "y")
-                //{
-                //    break;
-                //}
-
-
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
